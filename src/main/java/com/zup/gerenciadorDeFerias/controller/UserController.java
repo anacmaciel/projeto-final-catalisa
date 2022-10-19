27413923afe140171fcafb8a2daee3ea8a480748
k3vinrich4rd <kevinrichardve@gmail.com>
2022-10-19 13:18:39 -0300

    Inserindo implementação inicial na classe 'UserController'

--- src/main/java/com/zup/gerenciadorDeFerias/controller/UserController.java
+++ src/main/java/com/zup/gerenciadorDeFerias/controller/UserController.java
@@ -1,7 +1,14 @@
 package com.zup.gerenciadorDeFerias.controller;
 
+import com.zup.gerenciadorDeFerias.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path = "/users")
 public class UserController {
+
+    @Autowired
+    UserService userService;
 }
