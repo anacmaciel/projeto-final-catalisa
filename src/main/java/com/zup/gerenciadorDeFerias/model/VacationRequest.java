fb9d115ca09c9b16fc1d5fc82bd1e172934c7443
jhstavares <jhstavares_01@hotmail.com>
2022-10-19 15:39:32 -0300

    Inserindo implentações na classe 'VacationRequest'

--- src/main/java/com/zup/gerenciadorDeFerias/model/VacationRequest.java
+++ src/main/java/com/zup/gerenciadorDeFerias/model/VacationRequest.java
@@ -1,4 +1,45 @@
 package com.zup.gerenciadorDeFerias.model;
 
-public class VacationRequest {
+import com.zup.gerenciadorDeFerias.enumeration.StatusVacationRequest;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "vacation_request")
+
+public class VacationRequest implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(nullable = false)
+    private Integer vacationDays;
+
+    @Column(nullable = false)
+    private LocalDate starTat;
+
+    @Column(nullable = false)
+    private LocalDate endAt;
+
+    @Enumerated(EnumType.STRING)
+    private StatusVacationRequest statusVacationRequest;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+
 }
