5b2175de79506afec169389350c78c8ff2b70386
jhstavares <jhstavares_01@hotmail.com>
2022-10-19 15:26:59 -0300

    Implementando mensageam de validação na classe 'VacationResponsetDto'

--- src/main/java/com/zup/gerenciadorDeFerias/dto/VacationResponseDto.java
+++ src/main/java/com/zup/gerenciadorDeFerias/dto/VacationResponseDto.java
@@ -1,4 +1,26 @@
 package com.zup.gerenciadorDeFerias.dto;
 
-public class VacationReponseDto {
+import com.zup.gerenciadorDeFerias.enumeration.StatusVacationRequest;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VacationResponseDto {
+
+
+    private Long userId;
+
+    private Integer vacationDays;
+
+    private LocalDate starTat;
+
+    private LocalDate endAt;
+
+    private StatusVacationRequest statusVacationRequest;
+
 }
