4b00bed6647a91da38b90194edab76b35cf21c51
k3vinrich4rd <kevinrichardve@gmail.com>
2022-10-19 13:19:13 -0300

    Inserindo implementação inicial na interface 'UserRepository'

--- src/main/java/com/zup/gerenciadorDeFerias/repository/UserRepository.java
+++ src/main/java/com/zup/gerenciadorDeFerias/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package com.zup.gerenciadorDeFerias.repository;
 
+import com.zup.gerenciadorDeFerias.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
