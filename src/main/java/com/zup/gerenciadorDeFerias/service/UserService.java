b2084d1c9c56316059076db8e08e6b4fd6ad690d
k3vinrich4rd <kevinrichardve@gmail.com>
2022-10-19 19:35:18 -0300

    Inserindo implementações na classe 'UserService'

--- src/main/java/com/zup/gerenciadorDeFerias/service/UserService.java
+++ src/main/java/com/zup/gerenciadorDeFerias/service/UserService.java
@@ -1,12 +1,42 @@
 package com.zup.gerenciadorDeFerias.service;
 
+import com.zup.gerenciadorDeFerias.model.User;
 import com.zup.gerenciadorDeFerias.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    public User registerUser(User user) {
+        return userRepository.save(user);
+
+    }
+
+    public List<User> displayRegisteredUsers() {
+        return userRepository.findAll();
+    }
+
+
+    public Optional<User> displayUsersById(Long id) {
+        return userRepository.findById(id);
+    }
+
+
+
+    public User changeRegisteredUsers(User user) {
+        return userRepository.save(user);
+    }
+
+
+
+//    public Optional<User> changeCharacter(Long id) {
+//        return userRepository.findById(id);
+//    }
 }
