0b2b97c3e3277b2706890316939767668f15ca11
k3vinrich4rd <kevinrichardve@gmail.com>
2022-10-19 13:20:09 -0300

    Inserindo implementação inicial na classe 'UserService'

--- src/main/java/com/zup/gerenciadorDeFerias/service/UserService.java
+++ src/main/java/com/zup/gerenciadorDeFerias/service/UserService.java
@@ -1,7 +1,12 @@
 package com.zup.gerenciadorDeFerias.service;
 
+import com.zup.gerenciadorDeFerias.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
 }
