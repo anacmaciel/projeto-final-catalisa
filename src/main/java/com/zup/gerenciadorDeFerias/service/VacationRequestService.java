396710369986f0ddc8fa09ed481a3f4d10cccaea
k3vinrich4rd <kevinrichardve@gmail.com>
2022-10-19 19:35:50 -0300

    Inserindo implementações na classe 'VacationRequestService'

--- src/main/java/com/zup/gerenciadorDeFerias/service/VacationRequestService.java
+++ src/main/java/com/zup/gerenciadorDeFerias/service/VacationRequestService.java
@@ -1,12 +1,36 @@
 package com.zup.gerenciadorDeFerias.service;
 
+import com.zup.gerenciadorDeFerias.model.VacationRequest;
 import com.zup.gerenciadorDeFerias.repository.VacationRequestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class VacationRequestService {
 
     @Autowired
     VacationRequestRepository vacationRequestRepository;
+
+    public VacationRequest registerVacationRequest(VacationRequest vacationRequest) {
+        return vacationRequestRepository.save(vacationRequest);
+    }
+
+    public List<VacationRequest> viewRegisteredVacations() {
+        return vacationRequestRepository.findAll();
+    }
+
+    public Optional<VacationRequest> displayVacationRequestById(Long id) {
+        return vacationRequestRepository.findById(id);
+    }
+
+    public VacationRequest changeRegisteredVacationRequest(VacationRequest vacationRequest) {
+        return vacationRequestRepository.save(vacationRequest);
+    }
+
+//    public Object changeCharacter(Long id) {
+//
+//    }
 }
