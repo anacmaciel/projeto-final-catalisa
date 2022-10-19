06c9805ffff4c4b9807314af1de3b51f22c92f57
k3vinrich4rd <kevinrichardve@gmail.com>
2022-10-19 19:33:39 -0300

    Inserindo impplementações de classe 'UserController'

--- src/main/java/com/zup/gerenciadorDeFerias/controller/UserController.java
+++ src/main/java/com/zup/gerenciadorDeFerias/controller/UserController.java
@@ -1,9 +1,18 @@
 package com.zup.gerenciadorDeFerias.controller;
 
+import com.zup.gerenciadorDeFerias.dto.UserRequestDto;
+import com.zup.gerenciadorDeFerias.dto.UserResponseDto;
+import com.zup.gerenciadorDeFerias.model.User;
 import com.zup.gerenciadorDeFerias.service.UserService;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/users")
@@ -11,4 +20,33 @@ public class UserController {
 
     @Autowired
     UserService userService;
+
+
+    @PostMapping
+    public ResponseEntity<User> registerUser(@Valid @RequestBody User user) {
+        return ResponseEntity.ok(userService.registerUser(user));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> displayRegisteredUsers() {
+        return ResponseEntity.ok(userService.displayRegisteredUsers());
+    }
+
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Optional<User>> displayUsersById(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.displayUsersById(id));
+    }
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<User> changeRegisteredUsers(@RequestBody User user) {
+        return ResponseEntity.ok(userService.changeRegisteredUsers(user));
+    }
+
+//    @PatchMapping(path = "/{id}")
+//    public ResponseEntity<User> changeCharacter(@PathVariable Long id){
+//        return ResponseEntity.ok(userService.changeCharacter(id));
+//    }
+
+
 }
