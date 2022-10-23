0383f25d44c15457635514c1b5a54e2b4e79c2ab
AI-QA-paiva <pedropaiva.62124@gmail.com>
2022-10-23 10:58:47 -0300

    atualizando espacos vazios

--- src/main/java/com/zup/gerenciadorDeFerias/exception/ObjectNotFoundException.java
+++ src/main/java/com/zup/gerenciadorDeFerias/exception/ObjectNotFoundException.java
@@ -8,6 +8,7 @@ public class ObjectNotFoundException extends RuntimeException {
     }
 
     public ObjectNotFoundException(String message, Throwable cause) {
+
         super(message, cause);
     }
 
