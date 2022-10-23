086d0fae8d15e149542cbe5ef4244f9228bd163b
AI-QA-paiva <pedropaiva.62124@gmail.com>
2022-10-23 10:48:22 -0300

    atualizando metodo post do User

--- src/main/java/com/zup/gerenciadorDeFerias/controller/VacationRequestController.java
+++ src/main/java/com/zup/gerenciadorDeFerias/controller/VacationRequestController.java
@@ -1,5 +1,6 @@
 package com.zup.gerenciadorDeFerias.controller;
 
+import com.zup.gerenciadorDeFerias.dto.VacationRequestDto;
 import com.zup.gerenciadorDeFerias.model.VacationRequest;
 import com.zup.gerenciadorDeFerias.service.VacationRequestService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,7 +30,7 @@ public class VacationRequestController {
 
 
     @GetMapping(path = "/{id}")
-    public ResponseEntity<Optional<VacationRequest>> displayVacationRequestById(@PathVariable Long id) {
+    public ResponseEntity<Optional<VacationRequestDto>> displayVacationRequestById(@PathVariable Long id) {
         return ResponseEntity.ok(vacationRequestService.displayVacationRequestById(id));
     }
 
---
+++ src/main/java/com/zup/gerenciadorDeFerias/exception/BadRequest.java
@@ -0,0 +1,2 @@
+package com.zup.gerenciadorDeFerias.exception;public class BadRequest {
+}
