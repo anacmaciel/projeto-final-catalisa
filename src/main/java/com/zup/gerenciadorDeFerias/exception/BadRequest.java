9bfd807843ddd4e441e8c919fcd69f537cd68e35
AI-QA-paiva <pedropaiva.62124@gmail.com>
2022-10-23 10:56:18 -0300

    incluindo metodo novo na exception para tratar badRequest para data nascimento e data de contrataçao

--- src/main/java/com/zup/gerenciadorDeFerias/exception/BadRequest.java
+++ src/main/java/com/zup/gerenciadorDeFerias/exception/BadRequest.java
@@ -1,2 +1,15 @@
-package com.zup.gerenciadorDeFerias.exception;public class BadRequest {
+package com.zup.gerenciadorDeFerias.exception;
+
+public class BadRequest extends RuntimeException{
+
+    private static final long serialVersionUID = 1L;
+
+    public BadRequest(String message) {
+        super(message);
+    }
+
+//    public BadRequest(String message, Throwable cause) {
+//
+//        super(message, cause);
+//    }
 }
