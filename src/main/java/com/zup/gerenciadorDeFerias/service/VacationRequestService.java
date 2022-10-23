7e8c0a4ccea8c4908bdd14598a5d5aaa7fb6e24e
AI-QA-paiva <pedropaiva.62124@gmail.com>
2022-10-23 11:01:36 -0300

    atualizando metodo get para consulta findById da classe vacationRequest

--- src/main/java/com/zup/gerenciadorDeFerias/service/VacationRequestService.java
+++ src/main/java/com/zup/gerenciadorDeFerias/service/VacationRequestService.java
@@ -1,5 +1,7 @@
 package com.zup.gerenciadorDeFerias.service;
 
+import com.zup.gerenciadorDeFerias.dto.VacationRequestDto;
+//import com.zup.gerenciadorDeFerias.dto.VacationResponseDto;
 import com.zup.gerenciadorDeFerias.model.VacationRequest;
 import com.zup.gerenciadorDeFerias.repository.VacationRequestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,8 +24,20 @@ public class VacationRequestService {
         return vacationRequestRepository.findAllStatusVacationRequest();
     }
 
-    public Optional<VacationRequest> displayVacationRequestById(Long id) {
-        return vacationRequestRepository.findById(id);
+
+
+    //se usuario está com status INACTIVE nao pode fazer buscas - atraves do login, checar se esta ativo, se estiver inativo, desabilita o botao de pequisa
+    //usuario nao pode ter acesso a nenhum outro usuario - logica seria via seu acesso, Login/passorwd ja setar esse id nao deixar ele setar o id
+    public Optional<VacationRequestDto> displayVacationRequestById(Long id) {
+
+        VacationRequest vacationRequest= vacationRequestRepository.findById(id).get();
+        VacationRequestDto objDto = new VacationRequestDto(
+                vacationRequest.getVacationDays(),vacationRequest.getStartAt(),vacationRequest.getEndAt(),
+                vacationRequest.getStatusVacationRequest(), vacationRequest.getUser());
+        return Optional.of(objDto);
+
+      // return vacationRequestRepository.findById(id);
+
     }
 
     public VacationRequest changeRegisteredVacationRequest(VacationRequest vacationRequest) {
