e96b722ed189b6605479f475abc02c1851f1b06d
AI-QA-paiva <pedropaiva.62124@gmail.com>
2022-10-26 21:47:41 -0300

    criada estrutura basica do pacote Security com a classe securitConfing.

--- src/main/java/com/zup/gerenciadorDeFerias/security/SecurityConfig.java
+++ src/main/java/com/zup/gerenciadorDeFerias/security/SecurityConfig.java
@@ -1,2 +1,30 @@
-package com.zup.gerenciadorDeFerias.security;public class SecurityConfig {
-}
+//package com.zup.gerenciadorDeFerias.security;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+//@EnableWebSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        //http.csrf().disable();//.authorizeHttpRequests().antMatchers().permitAll().and().httpBasic();
+//    }
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+////        auth.inMemoryAuthentication()
+////                .withUser("joyce").password(passwordEncoder().encode("1234")).roles("ADMIN").and()
+////                .withUser("maria").password(passwordEncoder().encode("0000")).roles("USER");
+//    }
+//
+////    @Bean
+////    public BCryptPasswordEncoder passwordEncoder(){
+////        return new BCryptPasswordEncoder();
+////    }
+//
+//}
