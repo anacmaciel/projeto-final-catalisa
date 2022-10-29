87f7a7612a9154985e69d3431d114bbd4c697fbb
k3vinrich4rd <kevinrichardve@gmail.com>
2022-10-29 10:13:29 -0300

    Inserindo a anotação '@CrossOrigin' na camada rest da classe 'VacationRequestController'

--- src/main/java/com/zup/gerenciadorDeFerias/controller/VacationRequestController.java
+++ src/main/java/com/zup/gerenciadorDeFerias/controller/VacationRequestController.java
@@ -20,28 +20,33 @@ public class VacationRequestController {
     @Autowired
     VacationRequestService vacationRequestService;
 
+    @CrossOrigin(origins = "http://127.0.0.1:5500")
     @PostMapping
     public ResponseEntity<VacationResponseDto> registerVacationRequest(@Valid @RequestBody VacationRequestDto vacationRequestDto) {
         VacationResponseDto vacationResponseDto = vacationRequestService.registerVacationRequest(vacationRequestDto);
         return new ResponseEntity<>(vacationResponseDto, HttpStatus.CREATED);
     }
 
+    @CrossOrigin(origins = "http://127.0.0.1:5500")
     @GetMapping
     public ResponseEntity<List<VacationRequest>> viewRegisteredVacations() {
         return ResponseEntity.ok(vacationRequestService.viewRegisteredVacations());
     }
 
+    @CrossOrigin(origins = "http://127.0.0.1:5500")
     @GetMapping(path = "/{id}")
     public ResponseEntity<VacationRequest> displayVacationRequestById(@PathVariable Long id) {
         return ResponseEntity.ok(vacationRequestService.displayVacationRequestById(id));
     }
 
 
+    @CrossOrigin(origins = "http://127.0.0.1:5500")
     @PutMapping(path = "/{id}")
     public ResponseEntity<VacationResponseDto> changeRegisteredVacationRequest(@PathVariable Long id, @RequestBody @Valid VacationUpdateDto vacationUpdateDto) {
         return ResponseEntity.ok(vacationRequestService.changeRegisteredVacationRequest(id, vacationUpdateDto));
     }
 
+    @CrossOrigin(origins = "http://127.0.0.1:5500")
     @DeleteMapping(path = "/cancel/{id}")
     public void cancelRegisteredVacationRequest(@PathVariable Long id) {
         vacationRequestService.cancelRegisteredVacationRequest(id);
